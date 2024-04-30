dc7a3edd26705a97e13753506076b03e3e901a60
Notargg <gabriel.graton@estudante.ufscar.br>
2024-04-30 10:55:04 -0300

    T3 - Inicio
    - target feito
    - base do projeto feita

---
+++ T3/t3/src/main/java/br/ufscar/dc/compiladores/t3/Principal.java
@@ -0,0 +1,21 @@
+package br.ufscar.dc.compiladores.t3;
+
+import br.ufscar.dc.compiladores.t3.AlgumaParser.ProgramaContext;
+import java.io.IOException;
+import java.io.PrintWriter;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+
+public class Principal {
+    public static void main(String args[]) throws IOException {
+        CharStream cs = CharStreams.fromFileName(args[0]);
+        AlgumaLexer lexer = new AlgumaLexer(cs);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        AlgumaParser parser = new AlgumaParser(tokens);
+        ProgramaContext arvore = parser.programa();
+        AlgumaSemantico as = new AlgumaSemantico();
+        as.visitPrograma(arvore);
+        AlgumaSemanticoUtils.errosSemanticos.forEach((s) -> System.out.println(s));
+    }
+}
